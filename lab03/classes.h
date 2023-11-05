443d5138a1a6d2def972723e9f2cf83422aed504
matthewtop <tolpamateusz@gmail.com>
2023-11-05 02:46:34 +0100

    new functions

--- lab03/classes.h
+++ lab03/classes.h
@@ -5,19 +5,61 @@
 #ifndef LAB03_CLASSES_H
 #define LAB03_CLASSES_H
 
+#include <iostream>
+
+using namespace std;
+
 template <typename T>
-class Node{
+class Tree{
 public:
+    Tree() {}
+
     T key;
-    Node *left, *right;
-    Node *parent;
+    Tree *left;
+    Tree *right;
+    Tree *parent;
 
 
-};
+    Tree(T key) : key(key), left(nullptr), right(nullptr), parent(nullptr) {}
+    //Tree()= default;
+    ~Tree()= default;
 
-template<typename T>
-class BSTTree{
-public:
+    void dodaj(T key) {
+        if (key < this->key) {
+            if (left == nullptr) {
+                left = new Tree(key);
+                left->parent = this;
+            } else {
+                left->dodaj(key);
+            }
+        } else if (key > this->key) {
+            if (right == nullptr) {
+                right = new Tree(key);
+                right->parent = this;
+            } else {
+                right->dodaj(key);
+            }
+        }
+    }
+
+    T getWartosc(){
+        T wartosc;
+        cout<<"Podaj wartosc do wprowadzenia: ";
+        cin>>wartosc;
+        return wartosc;
+    }
+
+    void showTree(){
+        if (left != nullptr) {
+            left->showTree();
+        }
+        cout << key << " ";
+        if (right != nullptr) {
+            right->showTree();
+        }
+    }
+
+
 
 };
 
--- lab03/main.cpp
+++ lab03/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include "View.h"
+#include "classes.h"
+
+using namespace std;
 
 int main() {
-    View::mainMenu();
+
+    Tree<int> tree(10);  // Inicjalizacja drzewa z korzeniem o kluczu 50
+
+    int wybor;
+    bool dzialanie = true;
+    do {
+        View::mainMenu();
+        cin >> wybor;
+        switch (wybor) {
+            case 1:
+                tree.dodaj(tree.getWartosc());
+                break;
+            case 2:
+
+        }
+    } while (dzialanie);
 }
