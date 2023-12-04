3f493aa3f743209ae03d4d8f4494180ca6ba6849
matthewtop <tolpamateusz@gmail.com>
2023-12-04 02:15:24 +0100

    updated red black tree

---
+++ Lab04RedBlackTree/main.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include "View.h"
+#include "Classes.h"
+
+using namespace std;
+
+int main(){
+
+    RBT * rbt = new RBT();
+
+    int wybor;
+    bool dzialanie= true;
+    do{
+        View::mainMenu();
+        cin>>wybor;
+        switch (wybor) {
+            case 1:{
+                clock_t t1 = clock();
+                //todo:
+                clock_t t2 = clock();
+                double seconds = (t2 - t1) / (double) CLOCKS_PER_SEC;
+                double miliseconds = seconds*1000;
+                cout << miliseconds << " ms" << endl;
+                break;
+            }
+            case 2:{
+                clock_t t1 = clock();
+                rbt->preOrder(rbt->root);
+                cout<<endl;
+                clock_t t2 = clock();
+                double seconds = (t2 - t1) / (double) CLOCKS_PER_SEC;
+                double miliseconds = seconds*1000;
+                cout << miliseconds << " ms" << endl;
+                break;
+            }
+            case 3:{
+                clock_t t1 = clock();
+                rbt->inOrder(rbt->root);
+                cout<<endl;
+                clock_t t2 = clock();
+                double seconds = (t2 - t1) / (double) CLOCKS_PER_SEC;
+                double miliseconds = seconds*1000;
+                cout << miliseconds << " ms" << endl;
+                break;
+            }
+            case 4:{
+                clock_t t1 = clock();
+                rbt->clearTree();
+                clock_t t2 = clock();
+                double seconds = (t2 - t1) / (double) CLOCKS_PER_SEC;
+                double miliseconds = seconds*1000;
+                cout << miliseconds << " ms" << endl;
+                break;
+            }
+            case 5:{
+                cout<<rbt->getHeight()<<endl;
+                break;
+            }
+            case 6:{
+                int wartosc;
+                cout<<"Podaj wartosc: ";
+                cin>>wartosc;
+                rbt->insert(wartosc);
+                break;
+            }
+            case 7:{
+                clock_t t1 = clock();
+                rbt->display();
+                clock_t t2 = clock();
+                double seconds = (t2 - t1) / (double) CLOCKS_PER_SEC;
+                double miliseconds = seconds*1000;
+                cout << miliseconds << " ms" << endl;
+                break;
+            }
+            case 8:{
+                clock_t t1 = clock();
+                rbt->leftRotate(rbt->root);
+                clock_t t2 = clock();
+                double seconds = (t2 - t1) / (double) CLOCKS_PER_SEC;
+                double miliseconds = seconds*1000;
+                cout << miliseconds << " ms" << endl;
+                break;
+            }
+            case 9:{
+                clock_t t1 = clock();
+                rbt->rightRotate(rbt->root);
+                clock_t t2 = clock();
+                double seconds = (t2 - t1) / (double) CLOCKS_PER_SEC;
+                double miliseconds = seconds*1000;
+                cout << miliseconds << " ms" << endl;
+                break;
+            }
+            case 10:{
+                dzialanie=false;
+                break;
+            }
+            default:{
+                cout<<"Nieprawidlowy wybor sprobuj ponownie"<<endl;
+                break;
+            }
+        }
+    }while(dzialanie);
+}
